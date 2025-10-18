b277bb7d3a5dd52dc871f2d8bc407afe05165957
zerocool5878 <zerocool5878@gmail.com>
2025-10-17 21:58:45 -0400

    Bump version to 1.0.3

--- exam_clone_tool_v2.py
+++ exam_clone_tool_v2.py
@@ -20,7 +20,7 @@ except ImportError:
     CAPTURE_AVAILABLE = False
 
 # Application version and update configuration
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 GITHUB_REPO = "zerocool5878/exam-clone-tool"
 
 # Import auto-updater
